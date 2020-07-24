edf18c83e9d0bb4c1a84d631e50fb8ad3b84e423
lguardado <lucas.guardado@gmail.com>
2020-07-23 22:31:08 -0300

    fix errors

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Aux from '../../hoc/Aux'
+
+const Layout = (props) => (
+    <Aux>
+        <div> toolbar, bla bla</div>
+        <main>pito</main>
+    </Aux>
+)
+
+export default Layout
