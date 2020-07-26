0415c1cc7c5e4b6b1a67ca3919fc37ba869e68c2
lguardado <lucas.guardado@gmail.com>
2020-07-26 14:32:24 -0300

    improving performance on modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backrdop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop visible={props.purchasing} backdropClicked={props.backdropClickHandler} />
-        <div className={classes.Modal}
-            style={{
-                opacity: !props.purchasing ? '0' : '1',
-                transform: !props.purchasing ? 'translateY(-100vh)' : 'translateY(0)'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
 
-export default modal
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.purchasing !== this.props.purchasing
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop visible={this.props.purchasing} backdropClicked={this.props.backdropClickHandler} />
+                <div className={classes.Modal}
+                    style={{
+                        opacity: !this.props.purchasing ? '0' : '1',
+                        transform: !this.props.purchasing ? 'translateY(-100vh)' : 'translateY(0)'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal
