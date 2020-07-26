3ca5d8fd3ebb00e6b7ecac4adfe71fdbe6c37575
lguardado <lucas.guardado@gmail.com>
2020-07-26 14:29:58 -0300

    refactor drawertoggle

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import hamburger from '../../../../assets/images/hamburger.png'
 import classes from './DrawerToggle.css'
 
 const drawerToggle = ( props ) => (
-        <div onClick={props.drawerToggleClicked}>
-            <img className={classes.img} src={hamburger} alt='menu'/>
+        <div className={classes.DrawerToggle} onClick={props.drawerToggleClicked}>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 
